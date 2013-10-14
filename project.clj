ca52c686ec7ebbc14fc1430fbd9bc5d52826a8e1
Michael Gaare <michael@clickscape.com>
2013-10-14 13:29:37 -0400

    Added some dev deps

--- project.clj
+++ project.clj
@@ -3,5 +3,8 @@
   :url "https://github.com/mgaare/cube2-crypto"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :profiles {:dev
+             {:source-paths ["dev"]
+              :dependencies [[org.clojars.jcrossley3/tools.namespace "0.2.4.1"]]}}
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [crypto-random "1.1.0"]])
